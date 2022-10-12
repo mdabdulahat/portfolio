def0a0310aa08657c4d650a395ab11510f63efb4
Abdul Ahad <amdabdul879@gmail.com>
2022-10-12 23:45:47 +0600

    added about tab-menu

--- script.js
+++ script.js
@@ -121,5 +121,29 @@ $(document).ready(function(){
 
       let observer = new IntersectionObserver(callback, options);
       observer.observe(skillContent[0])
+
+
+      //custom javascipt tab-menu
+      let tablinks = document.querySelectorAll(".tab-links");
+      let tabcontents = document.querySelectorAll(".tab-contents");
+      
+      function opentab(tabname){
+        for(tablink of tablinks){
+          tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+          tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+      }
+
+      tablinks.forEach(link => {
+        link.addEventListener('click',  (e) => {
+            e.preventDefault();
+            let val = link.innerText?.toLowerCase();
+            opentab(val)
+        })
+      })
     
 });
